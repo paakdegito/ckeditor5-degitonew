3f86266d4d02a9430e3f2d977a9f2cedc685569a
paakdegito <103932222+paakdegito@users.noreply.github.com>
2023-03-01 17:54:02 +0700

    init ckeditor for degitonew

---
+++ src/ckeditor.js
@@ -0,0 +1,85 @@
+/**
+ * @license Copyright (c) 2014-2023, CKSource Holding sp. z o.o. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+ */
+import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor.js';
+import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment.js';
+import AutoLink from '@ckeditor/ckeditor5-link/src/autolink.js';
+import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold.js';
+import DocumentList from '@ckeditor/ckeditor5-list/src/documentlist.js';
+import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials.js';
+import FontSize from '@ckeditor/ckeditor5-font/src/fontsize.js';
+import Heading from '@ckeditor/ckeditor5-heading/src/heading.js';
+import HorizontalLine from '@ckeditor/ckeditor5-horizontal-line/src/horizontalline.js';
+import Indent from '@ckeditor/ckeditor5-indent/src/indent.js';
+import IndentBlock from '@ckeditor/ckeditor5-indent/src/indentblock.js';
+import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic.js';
+import Link from '@ckeditor/ckeditor5-link/src/link.js';
+import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph.js';
+import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice.js';
+import RemoveFormat from '@ckeditor/ckeditor5-remove-format/src/removeformat.js';
+import SourceEditing from '@ckeditor/ckeditor5-source-editing/src/sourceediting.js';
+import SpecialCharacters from '@ckeditor/ckeditor5-special-characters/src/specialcharacters.js';
+import Subscript from '@ckeditor/ckeditor5-basic-styles/src/subscript.js';
+import Superscript from '@ckeditor/ckeditor5-basic-styles/src/superscript.js';
+import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation.js';
+import Title from '@ckeditor/ckeditor5-heading/src/title.js';
+import Underline from '@ckeditor/ckeditor5-basic-styles/src/underline.js';
+
+class Editor extends ClassicEditor {}
+
+// Plugins to include in the build.
+Editor.builtinPlugins = [
+	Alignment,
+	AutoLink,
+	Bold,
+	DocumentList,
+	Essentials,
+	FontSize,
+	Heading,
+	HorizontalLine,
+	Indent,
+	IndentBlock,
+	Italic,
+	Link,
+	Paragraph,
+	PasteFromOffice,
+	RemoveFormat,
+	SourceEditing,
+	SpecialCharacters,
+	Subscript,
+	Superscript,
+	TextTransformation,
+	Title,
+	Underline
+];
+
+// Editor configuration.
+Editor.defaultConfig = {
+	toolbar: {
+		items: [
+			'heading',
+			'|',
+			'fontSize',
+			'bold',
+			'italic',
+			'link',
+			'alignment',
+			'horizontalLine',
+			'bulletedList',
+			'numberedList',
+			'removeFormat',
+			'underline',
+			'|',
+			'outdent',
+			'indent',
+			'|',
+			'undo',
+			'redo',
+			'sourceEditing'
+		]
+	},
+	language: 'en'
+};
+
+export default Editor;
